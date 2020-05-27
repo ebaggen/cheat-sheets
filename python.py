3adeb2cae29465cc113f0fe6dc6e68e0f973a57a
Eric Baggen <eric.baggen@gmail.com>
2020-05-27 09:07:35 -0700

    Added python cheat sheet

---
+++ python.py
@@ -0,0 +1,30 @@
+'''
+DECORATORS 
+
+Decorators are functions that take another function as an argument, enabling
+consistent pre- and post-processing of a function
+'''
+def decorator(function):
+    print('Decorator called')
+
+    def wrapper(*args, **kwargs):
+        # Pre-processing happens here
+        print('Doing some stuff before calling the function')
+
+        # Calling the function argument actually runs the function that is being deocorated
+        function()
+
+        # Post-processing happens here
+        print('Doing some stuff after calling the function')
+
+    return wrapper
+
+# This is a decorator in use:
+@decorator
+def hello_world:
+    print('Hello world!')
+
+# output:
+# >>> Doing some stuff before calling the function
+# >>> Hello world!
+# >>> Doing some stuff after calling the function
